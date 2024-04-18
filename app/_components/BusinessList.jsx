a1111612202e9519eb48faca8bbbe8343f10bb43
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-04-18 08:11:46 +0600

    done with businessList

--- app/_components/BusinessList.jsx
+++ app/_components/BusinessList.jsx
@@ -7,33 +7,33 @@ const BusinessList = ({ businessList, title }) => {
     <div className="mt-5">
       <h2 className="font-bold text-[22px]">{title}</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5">
-        {businessList.length > 0 ? (
-          businessList.map((business, index) => (
-            <div
-              key={index}
-              className="shadow-md rounded-lg hover:shadow-lg cursor-pointer hover:shadow-primary hover:scale-105 transition-all ease-in-out"
-            >
-              <Image
-                src={business?.images[0].url}
-                alt={business.name}
-                width={500}
-                height={200}
-                className="h-[150px] md:h-[200px] object-cover rounded-lg"
-              />
-              <div className="flex flex-col items-baseline p-3 gap-1">
-                <h2 className="p-1 bg-purple-200 text-primary rounded-full px-2 text-[12px]">
-                  {business.category.name}
-                </h2>
-                <h2>{business.name}</h2>
-                <h2>{business.contactPerson}</h2>
-                <h2>{business.address}</h2>
-                <Button>Book Now</Button>
+        {businessList.length > 0
+          ? businessList.map((business, index) => (
+              <div
+                key={index}
+                className="shadow-md rounded-lg hover:shadow-lg cursor-pointer hover:shadow-primary hover:scale-105 transition-all ease-in-out"
+              >
+                <Image
+                  src={business?.images[0].url}
+                  alt={business.name}
+                  width={500}
+                  height={200}
+                  className="h-[150px] md:h-[200px] object-cover rounded-lg"
+                />
+                <div className="flex flex-col items-baseline p-3 gap-1">
+                  <h2 className="p-1 bg-purple-200 text-primary rounded-full px-2 text-[12px]">
+                    {business.category.name}
+                  </h2>
+                  <h2 className="font-bold text-lg">{business.name}</h2>
+                  <h2 className="text-primary">{business.contactPerson}</h2>
+                  <h2 className="text-gray-500 text-sm">{business.address}</h2>
+                  <Button className="rounded-lg mt-3">Book Now</Button>
+                </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <p>coming</p>
-        )}
+            ))
+          : [1, 2, 3, 4, 5, 6].map((item, index) => (
+              <div key={index} className="w-full h-[300px] bg-gray-200 rounded-lg flex items-center justify-center">   </div>
+            ))}
       </div>
     </div>
   );
