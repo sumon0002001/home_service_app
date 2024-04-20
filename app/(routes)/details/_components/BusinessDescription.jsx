7e56f3f82eb1f07ede80e1b0b326a6f2522ad7f2
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-04-20 16:21:07 +0600

    Add business description

--- app/(routes)/details/_components/BusinessDescription.jsx
+++ app/(routes)/details/_components/BusinessDescription.jsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
-const BusinessDescription = ({business}) => {
-  return <div>businessd description</div>;
+const BusinessDescription = ({ business }) => {
+  return (
+    business?.name && (
+      <div>
+        <h2 className="font-bold text-[25px]">Description</h2>
+        <p className="mt-4 text-lg text-gray-600">{business.about}</p>
+        <h2 className="font-bold text-[25px] mt-8">Gallery</h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
+          {business?.images?.map((item, index) => (
+            <Image
+              key={index}
+              src={item?.url}
+              alt="image"
+              width={700}
+              height={200}
+              className="rounded-lg"
+            />
+          ))}
+        </div>
+      </div>
+    )
+  );
 };
 
 export default BusinessDescription;
